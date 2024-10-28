0161d75acfac128cf39fc0fa7d3e5e57078fc10f
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-28 14:48:49 -0300

    Adicionando construtor de Curso e Getters da classe

--- git_203624/java_203624/src/Curso.java
+++ git_203624/java_203624/src/Curso.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -5,4 +6,21 @@ public class Curso{
     private String titulo;
     private String instrutor;
     private Set<Aluno> alunos = new HashSet<>();
+
+    public Curso(String titulo, String instrutor) {
+        this.titulo = titulo;
+        this.instrutor = instrutor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getInstrutor() {
+        return instrutor;
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
+    }
 }
