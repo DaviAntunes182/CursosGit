dbf974aff0a7ac04e0ea1184cdeecf6bdfd2e0c4
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-28 14:45:53 -0300

    Relacionando Curso e Aluno

--- git_203624/java_203624/src/Curso.java
+++ git_203624/java_203624/src/Curso.java
@@ -1,4 +1,8 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Curso{
     private String titulo;
     private String instrutor;
+    private Set<Aluno> alunos = new HashSet<>();
 }
