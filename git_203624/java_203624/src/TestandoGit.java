21a07dc72de828fe4160ca2e3d28aca870d16be5
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-28 13:08:15 -0300

    Criando um java para testar os commits realizados

---
+++ git_203624/java_203624/src/TestandoGit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TestandoGit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = sc.next();
+
+        System.out.printf("""
+                Olá %s!
+                """, nome);
+    }
+}
