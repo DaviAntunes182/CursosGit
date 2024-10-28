961a5cc42ca18f1748c388611ab06b23a9057769
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-28 13:31:31 -0300

    Testando colaboração - no principal

--- git_203624/java_203624/src/TestandoGit.java
+++ git_203624/java_203624/src/TestandoGit.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TestandoGit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite seu nome: ");
+        System.out.print("Digite seu nome aqui: ");
         String nome = sc.next();
 
         System.out.printf("""
